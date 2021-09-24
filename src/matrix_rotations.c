a6027d13f37f4929d685fb6bc7611e86d2a3f6f0
jpceia <joao.p.ceia@gmail.com>
2021-09-24 13:40:27 +0100

    using matrix_zeros instead of matrix_empty to build rotation matrices

--- src/matrix_rotations.c
+++ src/matrix_rotations.c
@@ -6,7 +6,7 @@
 /*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 07:57:48 by jceia             #+#    #+#             */
-/*   Updated: 2021/09/21 17:39:51 by jceia            ###   ########.fr       */
+/*   Updated: 2021/09/22 17:27:28 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,12 @@ t_matrix	*matrix3x3_rotationX(float theta)
 {
 	t_matrix	*M;
 
-	M = matrix_empty(3, 3);
+	M = matrix_zeros(3, 3);
 	if (!M)
 		return (NULL);
 	M->data[0] = 1.0;
-	M->data[1] = 0.0;
-	M->data[2] = 0.0;
-	M->data[3] = 0.0;
 	M->data[4] = cos(theta);
 	M->data[5] = -sin(theta);
-	M->data[6] = 0.0;
 	M->data[7] = sin(theta);
 	M->data[8] = cos(theta);
 	return (M);
@@ -38,17 +34,13 @@ t_matrix	*matrix3x3_rotationY(float theta)
 {
 	t_matrix	*M;
 
-	M = matrix_empty(3, 3);
+	M = matrix_zeros(3, 3);
 	if (!M)
 		return (NULL);
 	M->data[0] = cos(theta);
-	M->data[1] = 0.0;
 	M->data[2] = sin(theta);
-	M->data[3] = 0.0;
 	M->data[4] = 1.0;
-	M->data[5] = 0.0;
 	M->data[7] = -sin(theta);
-	M->data[8] = 0.0;
 	M->data[9] = cos(theta);
 	return (M);
 }
@@ -62,12 +54,8 @@ t_matrix	*matrix3x3_rotationZ(float theta)
 		return (NULL);
 	M->data[0] = cos(theta);
 	M->data[1] = -sin(theta);
-	M->data[2] = 0.0;
 	M->data[3] = sin(theta);
 	M->data[4] = cos(theta);
-	M->data[5] = 0.0;
-	M->data[6] = 0.0;
-	M->data[7] = 0.0;
 	M->data[8] = 1.0;
 	return (M);
 }
