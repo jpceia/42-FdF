db99d1a20b1822e4c526744792c54e1dd8e85dec
jpceia <joao.p.ceia@gmail.com>
2021-10-07 23:30:00 +0100

    corrected check for boundary conditions to plot a pixel

--- src/mlx_draw.c
+++ src/mlx_draw.c
@@ -6,21 +6,13 @@
 /*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 01:23:27 by jceia             #+#    #+#             */
-/*   Updated: 2021/10/06 07:06:42 by jceia            ###   ########.fr       */
+/*   Updated: 2021/10/07 23:28:19 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include "libft.h"
 
-/*
- * TODO
- * add_shape
- * is a function that accepts a double (distance) and a int (color) as
- * arguments, 0 will add no shading to the color, whilst 1 will make the
- * color completely dark. 0.5 will dim it halfway, and .25 a quarter way.
- * You get the point.
- */
 void	plot_pixel(t_mlx *data, t_vec2D p, t_vec3D color)
 {
 	int		i;
@@ -29,7 +21,7 @@ void	plot_pixel(t_mlx *data, t_vec2D p, t_vec3D color)
 
 	i = (int)(p.x + 0.5);
 	j = (int)(p.y + 0.5);
-	if (i < 0 || i > data->width || j < 0 || j > data->height)
+	if (i < 0 || i >= data->width || j < 0 || j >= data->height)
 		return ;
 	dst = data->addr + (j * data->line_length + i * (data->bits_per_pixel / 8));
 	*(unsigned int *)dst += create_trgb(color);
