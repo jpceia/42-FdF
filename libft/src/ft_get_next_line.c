476d6cbcd335e9cf906d3c12cb4432b319d9bc1d
jpceia <joao.p.ceia@gmail.com>
2021-10-07 23:47:10 +0100

    allocating space to string to hold the line, before checking other conditions, to avoid having cases where returned status=-1 has allocated memory and others that dont

--- libft/src/ft_get_next_line.c
+++ libft/src/ft_get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 19:10:07 by jceia             #+#    #+#             */
-/*   Updated: 2021/08/27 19:18:26 by jceia            ###   ########.fr       */
+/*   Updated: 2021/10/07 22:45:01 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int	ft_get_next_line(int fd, char **line)
 	int		nb;
 	char	c;
 
+	*line = ft_empty_string();
 	if (fd < 0 || fd >= RLIMIT_NOFILE || !line)
 		return (-1);
-	*line = ft_empty_string();
 	if (!*line)
 		return (-1);
 	while (1)
