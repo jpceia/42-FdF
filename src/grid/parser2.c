3b91d18c2560ba5c5f3586ed56b82e37abd614a2
jpceia <joao.p.ceia@gmail.com>
2021-10-08 02:04:13 +0100

    fix to avoid segfault when there is a misconfigured input file.

--- src/grid/parser2.c
+++ src/grid/parser2.c
@@ -6,7 +6,7 @@
 /*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 18:33:58 by jceia             #+#    #+#             */
-/*   Updated: 2021/10/08 01:22:31 by jceia            ###   ########.fr       */
+/*   Updated: 2021/10/08 02:02:32 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,8 +48,9 @@ t_grid	*list_to_grid(t_list *lst, t_grid **grid)
 				(*grid)->data[line_nr]))
 		{
 			ft_lstclear(&lst, free);
-			return (clean_exit(*grid, "Errow converting list to grid",
-					grid_clear, 0));
+			clean_exit(*grid, "Errow converting list to grid", grid_clear, 0);
+			*grid = NULL;
+			return (NULL);
 		}
 		node = node->next;
 		line_nr++;
